3b4cd5bd8a83b3f4f3cb709c01d5d2013617300e
Harlan Iverson <h.iverson@gmail.com>
2013-06-24 14:07:23 -0500

    added google analytics pageview tracking. changed front page image.

--- src/client/controllers.ts
+++ src/client/controllers.ts
@@ -9,6 +9,33 @@ var ProgrammingIndexController = Ember.Controller.extend({
   }
 });
 
+declare var _gaq:any;
+
+var ApplicationController_currentPathChanged:any = function () {
+  var page;
+  var e:any = Ember;
+
+  console.log("currentPathChanged");
+
+  // window.location gets updated later in the current run loop, so we will
+  // wait until the next run loop to inspect its value and make the call
+  // to track the page view
+  e.run.next(function() {
+    // Track the page in Google Analytics
+    if (!e.isNone(_gaq)) {
+      // Assume that if there is a hash component to the url then we are using
+      // the hash location strategy. Otherwise, we'll assume the history
+      // strategy.
+      page = window.location.hash.length > 0 ?
+             window.location.hash.substring(1) :
+             window.location.pathname;
+
+      // You need your Google Analytics code already loaded for _ga to be initialized
+      _gaq.push(['_trackPageview', page]);
+    }
+  });
+};
+
 var ApplicationController = Ember.Controller.extend({
   routeChanged: (function() {
     /*
@@ -21,5 +48,8 @@ var ApplicationController = Ember.Controller.extend({
       return _gaq.push(['_trackPageview', page]);
     });
   */
-  })
+  }),
+
+  currentPathChanged: ApplicationController_currentPathChanged.observes('currentPath')
+
 });
